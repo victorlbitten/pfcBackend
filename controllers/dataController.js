a4b73d5646a1176d688534a1fbbee90a9174e03a
victorlbitten <victorlbitten@gmail.com>
2020-08-02 20:48:20 -0300

    Mapeamento básico rolante

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -2,39 +2,54 @@ const axios = require('axios');
 const apisController = require('./apisController');
 const descriptionsController = require('./descriptionsController');
 const mappingFunction = require('../factory/mappingFunction');
+const sqlFactory = require('../factory/mysql.factory');
 
-exports.getData = async function (request, response) {
-	const apiName = request.query.name;
-	// TODO: Modificar quando suporte a autenticação
-	const authToken = request.headers.authorization || null;
+exports.getData = async (request, response) => {
+	const { apiId } = request.body;
 
-	apisController.getApiDataByName(apiName, request)
-		.then(async ({ id: apiId, url, method }) => {
-			const rawDataFromApi = await requestDataFromApi(method, authToken, url);
-			const {mapping: unparsedMappingProperties} = await descriptionsController
-				.getMappingByApiId(apiId, request);
-			const mappingProperties = JSON.parse(unparsedMappingProperties);
+	const apiData = await makeRequestToApi(apiId, request);
+	const appDescription = await getDescription(apiId, request);
 
-			const mappedData = mappingFunction.map(rawDataFromApi, mappingProperties);
-			response.send(mappedData);
-		})
-		.catch((error) => {
-			return response.status(400).json(error);
-		});
+	const mappedData = mappingFunction.map(appDescription, apiData);
+
+	response.status(200).send({'1': mappedData, '2': appDescription});
 }
 
-function requestDataFromApi(method, authToken, url) {
-	const authConfig = {Authorization: authToken}
-	return new Promise(resolve => {
-		const requestOptions = {
-			method,
-			headers: authConfig,
-			url
-		};
-		axios(requestOptions)
-			.then(({ data: apiResponse }) => {
-				resolve(apiResponse);
-			})
-			.catch((error) => console.log(error));
-	})
+async function makeRequestToApi (apiId, requestHandler) {
+	const queryString = "SELECT url FROM apis WHERE id = ?";
+	const url = await sqlFactory.runQuery(requestHandler, queryString, apiId);
+
+	//TODO: make request
+
+	const mockedApiData = {
+		data: {
+			user: {
+				name: 'Victor',
+				grades: [1,2,3,4],
+				sisters: [
+					{
+						name:'isadora', age:29, test: {hahatest: 'isa'}, pets: ['gato', 'catioro']
+					},
+					{
+						name:'ligia',age:25, test: {hahatest: 'liginha'}, pets: ['gato', 'catioro', 'passarito']
+					}
+				]
+			}
+		}
+	};
+
+	return mockedApiData;
+}
+
+async function getDescription (apiId, requestHandler) {
+	const queryString = "SELECT description FROM descriptions WHERE api_id = ?";
+	const descriptionData = await sqlFactory.runQuery(requestHandler, queryString, apiId);
+	return JSON.parse(descriptionData[0].description);
+}
+
+async function getApiDescription (apiId, requestHandler) {
+	const queryString = "SELECT description FROM apisDescription WHERE api_id = ?";
+	const descriptionData = await sqlFactory.runQuery(requestHandler, queryString, apiId);
+
+	return JSON.parse(descriptionData[0].description);
 }
